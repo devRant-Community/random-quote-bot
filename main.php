67194d5930c8be8f1a0cee91b8c1bc5f66d878f4
Skayo <jonasg.cool@gmail.com>
2017-04-21 11:58:33 +0200

    WIP - Added getQuote function

--- main.php
+++ main.php
@@ -2,16 +2,25 @@
 
 class Bot {
 	function __construct(){
-
+		// Some configuration for the getQuote function
+		if(ini_get("allow_url_fopen") != 1)
+			ini_set("allow_url_fopen", 1);
 	}
 
 	function post($msg){
-
+		// WIP - maybe I'll have to use a library...
 	}
 
 	function getQuote(){
+		// Get the JSON from an API
+		$json = file_get_contents('http://quotes.stormconsultancy.co.uk/random.json');
 
-	} 
+		// Decode it to an array
+		$array = json_decode($json, true);
+
+		// Return it
+		return $array;
+	}
 }
 
 ?>
