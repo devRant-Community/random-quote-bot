d021f110b406abd39831ac372305df2931612a22
Skayo <jonasg.cool@gmail.com>
2017-11-24 23:16:53 +0100

    UPDATE FUCK YEAH
    (still)

--- main.php
+++ main.php
@@ -22,7 +22,7 @@ class Bot {
 		$this->quoteids = json_decode($json, true);
 
 		// Get all quotes
-		$this->quotes = json_decode(file_get_contents('quotes.json'), true);
+		$this->quotes = json_decode(file_get_contents($quotesFile), true);
 
 		// Set vars
 		$this->token_id = $token_id;
