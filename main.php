e0b73682cb7c166e920dd014987574a4661b5d3b
Skayo <jonasg.cool@gmail.com>
2017-11-25 18:28:06 +0100

    Fixed this shit now

--- main.php
+++ main.php
@@ -51,19 +51,19 @@ class Bot {
 
 		// Make data useable for request
 		$postdata = http_build_query($postdata);
-
-		$url = 'https://www.devrant.com/api/devrant/rants';
+		$url = 'https://devrant.com/api/devrant/rants';
 
 		// Curl options
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_POST, 1);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
-		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 0);
 		curl_setopt($curl, CURLOPT_HEADER, 0);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 		// Execute curl and get response
 		$response = curl_exec($curl);
+		curl_close($curl);
 		return $response;
 	}
 
@@ -110,10 +110,13 @@ class Bot {
 
 		// Execute curl and get response
 		$response = json_decode(curl_exec($curl), true);
+		curl_close($curl);
 
 		$this->token_id = $response["auth_token"]["id"];
 		$this->token_key = $response["auth_token"]["key"];
 		$this->user_id = $response["auth_token"]["user_id"];
+
+		return $response;
 	}
 
 	function nextQuote() {
